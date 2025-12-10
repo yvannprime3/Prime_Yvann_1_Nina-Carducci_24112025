f6b37e40ea36942bd8be3b97523f8a3a86811efe
Yvann Prime <yvannprime3@gmail.com>
2025-12-10 12:07:48 +0100

    Modifications de la hiérarchie de h1, h2, h3 etc + ajout d'un 'title' à la page web

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -223,7 +223,7 @@
         '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
       $.each(tags, function(index, value) {
         tagItems += `<li class="nav-item active">
-                <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
+                <span class="nav-link" role="button" tabindex="0" data-images-toggle="${value}">${value}</span></li>`;
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
 
